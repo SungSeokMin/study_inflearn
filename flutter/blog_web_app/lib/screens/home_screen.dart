f9e530bcad65d44cabd090149c41f2488a769ca6
sungseokmin <jkl154527@gmail.com>
2024-02-02 11:41:21 +0900

    웹사이트 웹뷰에 띄우기 & 다큐멘테이션 읽어보기

--- flutter/blog_web_app/lib/screens/home_screen.dart
+++ flutter/blog_web_app/lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController();
+  WebViewController controller = WebViewController()..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
