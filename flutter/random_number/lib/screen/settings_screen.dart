b72b1b42c1943db57b069ae98d59cc5547c5411f
sungseokmin <jkl154527@gmail.com>
2024-02-06 15:10:38 +0900

    Slider 위젯 사용하기

--- flutter/random_number/lib/screen/settings_screen.dart
+++ flutter/random_number/lib/screen/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number/constant/color.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -8,11 +9,48 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text('settings screen'),
-    ));
+    return Scaffold(
+        backgroundColor: primaryColor,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Row(
+                    children: maxNumber
+                        .toInt()
+                        .toString()
+                        .split('')
+                        .map(
+                          (x) => Image.asset('asset/img/$x.png', width: 50.0, height: 70.0),
+                        )
+                        .toList(),
+                  ),
+                ),
+                Slider(
+                    value: maxNumber,
+                    min: 10000,
+                    max: 10000 * 100,
+                    onChanged: (double value) {
+                      setState(() => maxNumber = value);
+                    }),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(backgroundColor: redColor),
+                  child: const Text(
+                    '저장',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
