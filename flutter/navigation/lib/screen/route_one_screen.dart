5c3b79227b9ba2d25fda7e7fe65897a2ca9f069f
sungseokmin <jkl154527@gmail.com>
2024-02-07 16:24:25 +0900

    Argument 전달하기

--- flutter/navigation/lib/screen/route_one_screen.dart
+++ flutter/navigation/lib/screen/route_one_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_two_screen.dart';
 
 class RouteOneScreen extends StatelessWidget {
   const RouteOneScreen({super.key});
@@ -13,6 +14,21 @@ class RouteOneScreen extends StatelessWidget {
         },
         child: const Text('Pop'),
       ),
+      ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) {
+                return const RouteTwoScreen();
+              },
+              settings: const RouteSettings(
+                arguments: 789,
+              ),
+            ),
+          );
+        },
+        child: const Text('Push'),
+      )
     ]);
   }
 }
--- flutter/navigation/lib/screen/route_two_screen.dart
+++ flutter/navigation/lib/screen/route_two_screen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('RouteTwoScreen')),
-    );
+    final arguments = ModalRoute.of(context)!.settings.arguments;
+
+    print(arguments);
+
+    return Scaffold(
+        body: MainLayout(
+      title: 'Route Two Screen',
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Pop'),
+        )
+      ],
+    ));
   }
 }
