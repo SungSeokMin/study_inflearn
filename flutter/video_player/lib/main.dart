68cb82918fd6075e0a1fff382f1e148013fd1e89
sungseokmin <jkl154527@gmail.com>
2024-02-09 14:30:03 +0900

    동영상 플레이어 시작

---
+++ flutter/video_player/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/screen/home_screen.dart';
+
+void main() {
+  return runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
