79f10d5a5d34a62c8364b2650dd2914ecc0056c4
sungseokmin <jkl154527@gmail.com>
2024-02-13 13:23:28 +0900

    구글지도 사용해보기

--- flutter/attendance_check_now/lib/screen/home_screen.dart
+++ flutter/attendance_check_now/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,12 +9,61 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const LatLng companyLatLng = LatLng(37.5233273, 126.921252);
+
+  final CameraPosition initialPosition = const CameraPosition(target: companyLatLng, zoom: 15);
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('hi'),
+    return Scaffold(
+      appBar: renderAppBar(),
+      body: Column(
+        children: [
+          _CustomGoogleMap(initialPosition: initialPosition),
+          const _ChoolCheckButton(),
+        ],
       ),
+    );
+  }
+
+  AppBar renderAppBar() {
+    return AppBar(
+      title: const Text(
+        '오늘도 출근',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class _CustomGoogleMap extends StatelessWidget {
+  final CameraPosition initialPosition;
+
+  const _CustomGoogleMap({required this.initialPosition});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: GoogleMap(
+        initialCameraPosition: initialPosition,
+        mapType: MapType.normal,
+      ),
+    );
+  }
+}
+
+class _ChoolCheckButton extends StatelessWidget {
+  const _ChoolCheckButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(
+      child: Text('출근'),
     );
   }
 }
