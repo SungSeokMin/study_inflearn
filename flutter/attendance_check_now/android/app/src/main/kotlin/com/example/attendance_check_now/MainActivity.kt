edaacc89144416d0d800b0331808235ba943f2f2
sungseokmin <jkl154527@gmail.com>
2024-02-13 12:34:59 +0900

    출석체크 Now 시작

---
+++ flutter/attendance_check_now/android/app/src/main/kotlin/com/example/attendance_check_now/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.attendance_check_now
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/attendance_check_now/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:attendance_check_now/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
