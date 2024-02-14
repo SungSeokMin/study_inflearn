cd8e51f33e741a0fb9940b22d2a8d8bb2b71aca3
sungseokmin <jkl154527@gmail.com>
2024-02-14 15:00:48 +0900

    캘린더 스케쥴러 시작

---
+++ flutter/calendar_scheduler/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
+}
