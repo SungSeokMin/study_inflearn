49db4ceff81101d52b36bf3d4947a2a994cc7474
sungseokmin <jkl154527@gmail.com>
2024-02-15 17:09:30 +0900

    색상정보 DB에 넣기

--- flutter/calendar_scheduler/lib/database/drift_database.dart
+++ flutter/calendar_scheduler/lib/database/drift_database.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:calendar_scheduler/model/category_color.dart';
 import 'package:calendar_scheduler/model/schedule.dart';
+import 'package:calendar_scheduler/model/schedule_with_color.dart';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
@@ -17,6 +18,49 @@ part 'drift_database.g.dart';
 )
 class LocalDatabase extends _$LocalDatabase {
   LocalDatabase() : super(_openConnection());
+
+  Future<Schedule> getScheduleById(int id) =>
+      (select(schedules)..where((tbl) => tbl.id.equals(id))).getSingle();
+
+  Future<int> createSchedule(SchedulesCompanion data) => into(schedules).insert(data);
+
+  Future<int> createCategoryColor(CategoryColorCompanion data) => into(categoryColor).insert(data);
+
+  Future<List<CategoryColorData>> getCategoryColors() => select(categoryColor).get();
+
+  Future<int> updateScheduleById(int id, SchedulesCompanion data) =>
+      (update(schedules)..where((tbl) => tbl.id.equals(id))).write(data);
+
+  Future<int> removeSchedule(int id) => (delete(schedules)..where((tbl) => tbl.id.equals(id))).go();
+
+  Stream<List<ScheduleWithColor>> watchSchedules(DateTime date) {
+    final query = select(schedules)
+        .join([innerJoin(categoryColor, categoryColor.id.equalsExp(schedules.colorId))]);
+
+    query.where(schedules.date.equals(date));
+    query.orderBy(
+      [
+        // asc -> ascending 오름차순
+        // desc -> descending 내림차순
+        OrderingTerm.asc(schedules.startTime),
+      ],
+    );
+
+    return query.watch().map(
+          (rows) => rows
+              .map(
+                (row) => ScheduleWithColor(
+                  schedule: row.readTable(schedules),
+                  categoryColor: row.readTable(
+                      categoryColor as ResultSetImplementation<$CategoryColorTable, CategoryColor>),
+                ),
+              )
+              .toList(),
+        );
+  }
+
+  @override
+  int get schemaVersion => 1;
 }
 
 LazyDatabase _openConnection() {
--- flutter/calendar_scheduler/lib/main.dart
+++ flutter/calendar_scheduler/lib/main.dart
@@ -1,11 +1,34 @@
+import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:calendar_scheduler/screen/home_screen.dart';
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+const defaultColors = [
+  'F44336',
+  'FF9800',
+  'FFEB3B',
+  'FCAF50',
+  '2196F3',
+  '3F51B5',
+  '9C27B0',
+];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
+
+  final database = LocalDatabase();
+
+  final colors = await database.getCategoryColors();
+  if (colors.isEmpty) {
+    for (String hexCode in defaultColors) {
+      await database.createCategoryColor(CategoryColorCompanion(
+        hexCode: Value(hexCode),
+      ));
+    }
+  }
 
   return runApp(
     MaterialApp(
