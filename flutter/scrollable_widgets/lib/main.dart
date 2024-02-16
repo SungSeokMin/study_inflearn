4102ee45999417020476dcb58de4e150bcd65b1f
sungseokmin <jkl154527@gmail.com>
2024-02-16 11:25:39 +0900

    스크롤 위젯

---
+++ flutter/scrollable_widgets/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/screen/home_screen.dart';
+
+void main() {
+  return runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
