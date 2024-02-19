5186cb597a247c6f94a400fa890965a5da179b6a
sungseokmin <jkl154527@gmail.com>
2024-02-19 14:05:26 +0900

    ListView 뽀개버리기

--- flutter/scrollable_widgets/lib/screen/home_screen.dart
+++ flutter/scrollable_widgets/lib/screen/home_screen.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable_widgets/layout/main_layout.dart';
+import 'package:scrollable_widgets/screen/list_view_screen.dart';
 import 'package:scrollable_widgets/screen/single_child_scroll_view_screen.dart';
 
+class ScreenModel {
+  final WidgetBuilder builder;
+  final String title;
+
+  ScreenModel({
+    required this.builder,
+    required this.title,
+  });
+}
+
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final screens = [
+    ScreenModel(
+      builder: (_) => const SingleChildScrollViewScreen(),
+      title: 'SingleChildScrollViewScreen',
+    ),
+    ScreenModel(
+      builder: (_) => ListViewScreen(),
+      title: 'ListViewScreen',
+    ),
+  ];
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +36,20 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => SingleChildScrollViewScreen(),
-                    ),
-                  );
-                },
-                child: const Text('SingleChildScrollViewScreen'))
-          ],
+          children: screens
+              .map(
+                (screen) => ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: screen.builder,
+                      ),
+                    );
+                  },
+                  child: Text(screen.title),
+                ),
+              )
+              .toList(),
         ),
       ),
     );
