50ecd56852c5c97d584ff8208459cd3c3c1417e6
sungseokmin <jkl154527@gmail.com>
2024-02-20 19:36:09 +0900

    SliverAppBar 세팅하기

--- flutter/dusty_dust/lib/main.dart
+++ flutter/dusty_dust/lib/main.dart
@@ -2,7 +2,10 @@ import 'package:dusty_dust/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(const MaterialApp(
-    home: HomeScreen(),
+  return runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'sunflower',
+    ),
+    home: const HomeScreen(),
   ));
 }
