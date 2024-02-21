636e16ac0400addeb99567168d65c96f63690281
sungseokmin <jkl154527@gmail.com>
2024-02-21 22:35:59 +0900

    SliverAppBar에 데이터 적용하기

--- flutter/dusty_dust/lib/screen/home_screen.dart
+++ flutter/dusty_dust/lib/screen/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:dusty_dust/component/hourly_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
 import 'package:dusty_dust/const/colors.dart';
+import 'package:dusty_dust/const/status_level.dart';
+import 'package:dusty_dust/model/stat_model.dart';
 import 'package:dusty_dust/repository/stat_repository.dart';
 import 'package:flutter/material.dart';
 
@@ -14,39 +16,58 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    super.initState();
-
-    fetchData();
-  }
-
-  fetchData() async {
+  Future<List<StatModel>> fetchData() async {
     final statModels = await StatRepository.fetchData();
+
+    return statModels;
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: primaryColor,
-      drawer: MainDrawer(),
-      body: CustomScrollView(
-        slivers: [
-          MainAppBar(),
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                CategoryCard(),
-                SizedBox(
-                  height: 16.0,
+      drawer: const MainDrawer(),
+      body: FutureBuilder<List<StatModel>>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text(
+                  '에러가 있습니다.',
                 ),
-                HourlyCard(),
+              );
+            }
+
+            List<StatModel> stats = snapshot.data!;
+            StatModel recentStat = stats[0];
+
+            final status = statusLevel
+                .where(
+                  (element) => element.minFineDust < recentStat.seoul,
+                )
+                .last;
+
+            return CustomScrollView(
+              slivers: [
+                MainAppBar(
+                  stat: recentStat,
+                  status: status,
+                ),
+                const SliverToBoxAdapter(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      CategoryCard(),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      HourlyCard(),
+                    ],
+                  ),
+                )
               ],
-            ),
-          )
-        ],
-      ),
+            );
+          }),
     );
   }
 }
