c5df9938b62b2c996a0a698bfcddc14b273949d3
sungseokmin <jkl154527@gmail.com>
2024-02-21 09:15:46 +0900

    Drawer 디자인하기

--- flutter/dusty_dust/lib/component/main_app_bar.dart
+++ flutter/dusty_dust/lib/component/main_app_bar.dart
@@ -15,50 +15,55 @@ class MainAppBar extends StatelessWidget {
       expandedHeight: 500,
       backgroundColor: primaryColor,
       flexibleSpace: FlexibleSpaceBar(
-        background: SafeArea(
-          child: Column(
-            children: [
-              Text(
-                '서울',
-                style: ts.copyWith(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w700,
+        background: Container(
+          margin: const EdgeInsets.only(
+            top: kToolbarHeight,
+          ),
+          child: SafeArea(
+            child: Column(
+              children: [
+                Text(
+                  '서울',
+                  style: ts.copyWith(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              Text(
-                DateTime.now().toString(),
-                style: ts.copyWith(
-                  fontSize: 20.0,
+                Text(
+                  DateTime.now().toString(),
+                  style: ts.copyWith(
+                    fontSize: 20.0,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Image.asset(
-                'asset/img/mediocre.png',
-                width: MediaQuery.of(context).size.width / 2,
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                '보통',
-                style: ts.copyWith(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w700,
+                const SizedBox(
+                  height: 20.0,
                 ),
-              ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              Text(
-                '나쁘지 않네요!',
-                style: ts.copyWith(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w700,
+                Image.asset(
+                  'asset/img/mediocre.png',
+                  width: MediaQuery.of(context).size.width / 2,
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  '보통',
+                  style: ts.copyWith(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                Text(
+                  '나쁘지 않네요!',
+                  style: ts.copyWith(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
