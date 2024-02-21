ef84334fc5cd4efae65d0827083634c4ac670293
sungseokmin <jkl154527@gmail.com>
2024-02-21 11:01:55 +0900

    HourlyCard 정리하기

--- flutter/dusty_dust/lib/screen/home_screen.dart
+++ flutter/dusty_dust/lib/screen/home_screen.dart
@@ -1,7 +1,6 @@
-import 'package:dusty_dust/component/card_title.dart';
 import 'package:dusty_dust/component/category_card.dart';
+import 'package:dusty_dust/component/hourly_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
-import 'package:dusty_dust/component/main_card.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
 import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
@@ -11,64 +10,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: primaryColor,
-      drawer: const MainDrawer(),
+      drawer: MainDrawer(),
       body: CustomScrollView(
         slivers: [
-          const MainAppBar(),
+          MainAppBar(),
           SliverToBoxAdapter(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const CategoryCard(),
-                const SizedBox(
+                CategoryCard(),
+                SizedBox(
                   height: 16.0,
                 ),
-                MainCard(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      const CardTitle(title: '시간별 미세먼지'),
-                      Column(
-                        children: List.generate(
-                          24,
-                          (index) {
-                            final now = DateTime.now();
-                            final hour = now.hour;
-                            int currentHour = hour - index;
-
-                            if (currentHour < 0) currentHour += 24;
-
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0,
-                                vertical: 4.0,
-                              ),
-                              child: Row(
-                                children: [
-                                  Expanded(child: Text('$currentHour시')),
-                                  Expanded(
-                                    child: Image.asset(
-                                      'asset/img/good.png',
-                                      height: 20.0,
-                                    ),
-                                  ),
-                                  const Expanded(
-                                    child: Text(
-                                      '좋음',
-                                      textAlign: TextAlign.right,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        ).toList(),
-                      )
-                    ],
-                  ),
-                )
+                HourlyCard(),
               ],
             ),
           )
