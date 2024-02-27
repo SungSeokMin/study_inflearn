cd5e1dbb6ac2911c7d8b5a5b404d6f87f0c75adf
sungseokmin <jkl154527@gmail.com>
2024-02-27 13:51:13 +0900

    기본 탭바

--- flutter/tab_bar/lib/const/tabs.dart
+++ flutter/tab_bar/lib/const/tabs.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 class TabInfo {
   final IconData icon;
-  final String labe;
+  final String label;
 
   const TabInfo({
     required this.icon,
-    required this.labe,
+    required this.label,
   });
 }
 
 const TABS = [
-  TabInfo(icon: Icons.account_balance_outlined, labe: '지갑'),
-  TabInfo(icon: Icons.alarm_add_outlined, labe: '알람'),
-  TabInfo(icon: Icons.keyboard, labe: '키보드'),
-  TabInfo(icon: Icons.ac_unit, labe: '온도'),
-  TabInfo(icon: Icons.android, labe: '안드로이드'),
+  TabInfo(icon: Icons.account_balance_outlined, label: '지갑'),
+  TabInfo(icon: Icons.alarm_add_outlined, label: '알람'),
+  TabInfo(icon: Icons.keyboard, label: '키보드'),
+  TabInfo(icon: Icons.ac_unit, label: '온도'),
+  TabInfo(icon: Icons.android, label: '안드로이드'),
 ];
--- flutter/tab_bar/lib/screen/basic_appbar_tabbar_screen.dart
+++ flutter/tab_bar/lib/screen/basic_appbar_tabbar_screen.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:tab_bar/const/tabs.dart';
 
 class BasicAppbarTabbarScreen extends StatelessWidget {
   const BasicAppbarTabbarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [],
+    return DefaultTabController(
+      length: TABS.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('BasicAppBarScreen'),
+          bottom: TabBar(
+            tabs: TABS
+                .map(
+                  (tab) => Tab(
+                    icon: Icon(tab.icon),
+                    child: Text(tab.label),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+        body: const Column(
+          children: [],
+        ),
       ),
     );
   }
