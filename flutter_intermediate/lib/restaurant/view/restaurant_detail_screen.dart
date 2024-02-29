8c9e2e27ed73cbce54e34840cbe92a127a0217c5
sungseokmin <jkl154527@gmail.com>
2024-02-29 18:51:38 +0900

    SliverList 구현하기

--- flutter_intermediate/lib/restaurant/view/restaurant_detail_screen.dart
+++ flutter_intermediate/lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_intermediate/common/layout/default_layout.dart';
 import 'package:flutter_intermediate/product/component/product_card.dart';
 import 'package:flutter_intermediate/restaurant/component/restaurant_card.dart';
@@ -9,25 +10,60 @@ class RestaurantDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '떡볶이',
-            tags: const ['맛있는', '매콤한'],
-            ratings: 10,
-            ratingsCount: 2,
-            deliveryTime: 15,
-            deliveryFee: 22,
-            isDetail: true,
-            detail: '맛있는 떡볶이',
+        title: '불타는 떡볶이',
+        child: CustomScrollView(
+          slivers: [
+            renderTop(),
+            renderLabel(),
+            renderProducts(),
+          ],
+        ));
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '떡볶이',
+        tags: const ['맛있는', '매콤한'],
+        ratings: 10,
+        ratingsCount: 2,
+        deliveryTime: 15,
+        deliveryFee: 22,
+        isDetail: true,
+        detail: '맛있는 떡볶이',
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: ProductCard(),
-          ),
-        ],
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return const Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
       ),
     );
   }
