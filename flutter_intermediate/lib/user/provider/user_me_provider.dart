93b6b930e6e9a7a662724f95a4926fa6d70f6dca
sungseokmin <jkl154527@gmail.com>
2024-03-19 14:46:27 +0900

    UserMeProvider 마무리하기

--- flutter_intermediate/lib/user/provider/user_me_provider.dart
+++ flutter_intermediate/lib/user/provider/user_me_provider.dart
@@ -1,14 +1,17 @@
 import 'package:flutter_intermediate/common/const/data.dart';
 import 'package:flutter_intermediate/user/model/user_model.dart';
+import 'package:flutter_intermediate/user/repository/auth_repository.dart';
 import 'package:flutter_intermediate/user/repository/user_me_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
+  final AuthRepository authRepository;
   final UserMeRepository repository;
   final FlutterSecureStorage storage;
 
   UserMeStateNotifier({
+    required this.authRepository,
     required this.repository,
     required this.storage,
   }) : super(UserModelLoading()) {
@@ -27,5 +30,41 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
     final response = await repository.getMe();
 
     state = response;
+  }
+
+  Future<UserModelBase> login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      state = UserModelLoading();
+
+      final response = await authRepository.login(
+        username: username,
+        password: password,
+      );
+
+      await storage.write(key: refreshTokenKey, value: response.refreshToken);
+      await storage.write(key: accessTokenKey, value: response.accessToken);
+
+      final userResponse = await repository.getMe();
+
+      state = userResponse;
+
+      return userResponse;
+    } catch (err) {
+      state = UserModelError(message: '로그인에 실패했습니다.');
+
+      return Future.value(state);
+    }
+  }
+
+  Future<void> logout() async {
+    state = null;
+
+    await Future.wait([
+      storage.delete(key: accessTokenKey),
+      storage.delete(key: refreshTokenKey),
+    ]);
   }
 }
