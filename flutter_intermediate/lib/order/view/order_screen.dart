41ab381dc68fa801d8076469ee5fe8d47c4d9fd8
sungseokmin <jkl154527@gmail.com>
2024-03-25 16:53:13 +0900

    OrderPagination 작업하기

--- flutter_intermediate/lib/order/view/order_screen.dart
+++ flutter_intermediate/lib/order/view/order_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_intermediate/common/component/pagination_list_view.dart';
+import 'package:flutter_intermediate/order/component/order_card.dart';
+import 'package:flutter_intermediate/order/model/order_model.dart';
+import 'package:flutter_intermediate/order/provider/order_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class OrderScreen extends ConsumerWidget {
@@ -6,8 +10,9 @@ class OrderScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text('주문'),
+    return PaginationListView<OrderModel>(
+      provider: orderProvider,
+      itemBuilder: <OrderModel>(_, index, model) => OrderCard.fromModel(model: model),
     );
   }
 }
