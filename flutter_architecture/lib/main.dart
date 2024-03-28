49530cb870c8c076d56ff0d925c8fcf458ca5d2b
sungseokmin <jkl154527@gmail.com>
2024-03-28 16:39:05 +0900

    12 Provider

--- flutter_architecture/lib/main.dart
+++ flutter_architecture/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_architecture/data/photo_provider.dart';
 import 'package:flutter_architecture/data/pixabay_api.dart';
 import 'package:flutter_architecture/ui/home_screen.dart';
 import 'package:flutter_architecture/ui/home_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   return runApp(
     MaterialApp(
-      home: PhotoProvider(
-        viewModel: HomeViewModel(PixabayApi()),
+      home: Provider(
+        create: (_) => HomeViewModel(PixabayApi()),
         child: const HomeScreen(),
       ),
     ),
--- flutter_architecture/lib/ui/home_screen.dart
+++ flutter_architecture/lib/ui/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_architecture/data/photo_provider.dart';
 import 'package:flutter_architecture/model/photo_model.dart';
+import 'package:flutter_architecture/ui/home_view_model.dart';
 import 'package:flutter_architecture/ui/widget/photo_widget.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -24,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = PhotoProvider.of(context).viewModel;
+    final viewModel = context.watch<HomeViewModel>();
 
     return Scaffold(
       backgroundColor: Colors.white,
