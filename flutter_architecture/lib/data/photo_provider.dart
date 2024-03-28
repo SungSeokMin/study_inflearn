593f9ba14a7dfcd249af4bcbe68a8f32c64340f9
sungseokmin <jkl154527@gmail.com>
2024-03-28 11:23:58 +0900

    06 Stream을 활용하여 데이터 다루기

--- flutter_architecture/lib/data/photo_provider.dart
+++ flutter_architecture/lib/data/photo_provider.dart
@@ -1,22 +1,33 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_architecture/data/api.dart';
+import 'package:flutter_architecture/model/photo.model.dart';
 
 class PhotoProvider extends InheritedWidget {
   final PixabayApi api;
 
-  const PhotoProvider({
+  final _photoSteamController = StreamController<List<Photo>>()..add([]);
+
+  Stream<List<Photo>> get photoStream => _photoSteamController.stream;
+
+  PhotoProvider({
     super.key,
     required this.api,
     required super.child,
   });
 
   static PhotoProvider of(BuildContext context) {
-    final PhotoProvider? result =
-        context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
+    final PhotoProvider? result = context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
 
     assert(result != null, 'No PixabayApi found in context');
 
     return result!;
+  }
+
+  Future<void> fetch(String query) async {
+    final result = await api.fetch(query);
+    _photoSteamController.add(result);
   }
 
   @override
--- flutter_architecture/lib/ui/home_screen.dart
+++ flutter_architecture/lib/ui/home_screen.dart
@@ -15,8 +15,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final _controller = TextEditingController();
 
-  List<Photo> _photos = [];
-
   @override
   void dispose() {
     _controller.dispose();
@@ -53,36 +51,38 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 suffixIcon: IconButton(
                   onPressed: () async {
-                    final photos =
-                        await photoProvider.api.fetch(_controller.text);
-
-                    setState(() {
-                      _photos = photos;
-                    });
+                    photoProvider.fetch(_controller.text);
                   },
                   icon: const Icon(Icons.search),
                 ),
               ),
             ),
           ),
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(16),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-              ),
-              itemCount: _photos.length,
-              itemBuilder: (context, index) {
-                final photo = _photos[index];
+          StreamBuilder<List<Photo>>(
+              stream: photoProvider.photoStream,
+              builder: (context, snapshot) {
+                final photos = snapshot.data;
+                if (photos == null) return const CircularProgressIndicator();
 
-                return PhotoWidget(
-                  photo: photo,
+                return Expanded(
+                  child: GridView.builder(
+                    padding: const EdgeInsets.all(16),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                    ),
+                    itemCount: photos.length,
+                    itemBuilder: (context, index) {
+                      final photo = photos[index];
+
+                      return PhotoWidget(
+                        photo: photo,
+                      );
+                    },
+                  ),
                 );
-              },
-            ),
-          ),
+              }),
         ],
       ),
     );
