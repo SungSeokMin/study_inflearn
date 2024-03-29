1530313264fabb4b226d0e06b330b229f5deaa33
sungseokmin <jkl154527@gmail.com>
2024-03-29 12:22:17 +0900

    18 DI (의존성 주입)

--- flutter_architecture/lib/main.dart
+++ flutter_architecture/lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_architecture/data/data_source/pixabay_api.dart';
-import 'package:flutter_architecture/data/repository/photo_api_repository_impl.dart';
-import 'package:flutter_architecture/domain/use_case/get_photos_use_case.dart';
+import 'package:flutter_architecture/di/provider_setup.dart';
 import 'package:flutter_architecture/presentation/home/home_screen.dart';
-import 'package:flutter_architecture/presentation/home/home_view_model.dart';
-import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
-      home: ChangeNotifierProvider(
-        create: (_) =>
-            HomeViewModel(GetPhotosUseCase(PhotoApiRepositoryImpl(PixabayApi(http.Client())))),
-        child: const HomeScreen(),
+    MultiProvider(
+      providers: globalProviders,
+      child: const MaterialApp(
+        home: HomeScreen(),
       ),
     ),
   );
