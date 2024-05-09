b091bc942a28e5546e663210caf92fb19f2b9d67
sungseokmin <jkl154527@gmail.com>
2024-05-09 12:51:10 +0900

    remove: 아키텍처

--- flutter_architecture/lib/main.dart
+++ flutter_architecture/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_architecture/di/provider_setup.dart';
-import 'package:flutter_architecture/presentation/home/home_screen.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   return runApp(
-    MultiProvider(
-      providers: globalProviders,
-      child: const MaterialApp(
-        home: HomeScreen(),
-      ),
+    const MaterialApp(
+      home: HomePage(),
     ),
   );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
