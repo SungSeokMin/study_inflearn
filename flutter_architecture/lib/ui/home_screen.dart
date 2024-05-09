766936be15fd891d6d7b27f6ee824f93c757674d
sungseokmin <jkl154527@gmail.com>
2024-05-09 14:18:31 +0900

    3. 이미지 검색 기본 버전 완성

--- flutter_architecture/lib/ui/home_screen.dart
+++ flutter_architecture/lib/ui/home_screen.dart
@@ -1,7 +1,39 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_architecture/model/photo.dart';
+import 'package:flutter_architecture/ui/widget/photo_widget.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final controller = TextEditingController();
+
+  List<Photo> photos = [];
+
+  Future<List<Photo>> fetch(String query) async {
+    final response = await http.get(
+      Uri.parse(
+          'https://pixabay.com/api/?key=23377613-9fc7315240ffa4544f10f1e82&q=$query&image_type=photo&pretty=true'),
+    );
+
+    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    Iterable hits = jsonResponse['hits'];
+    return hits.map((hit) => Photo.fromJson(hit)).toList();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +62,7 @@ class HomeScreen extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: TextField(
+        controller: controller,
         decoration: InputDecoration(
           suffixIcon: buildIconButton(),
           border: const OutlineInputBorder(
@@ -44,8 +77,11 @@ class HomeScreen extends StatelessWidget {
 
   IconButton buildIconButton() {
     return IconButton(
-      onPressed: () {
-        print('[home_screen.dart, 28] : hi');
+      onPressed: () async {
+        final photos = await fetch(controller.text);
+        setState(() {
+          this.photos = photos;
+        });
       },
       icon: const Icon(Icons.search),
     );
@@ -60,21 +96,11 @@ class HomeScreen extends StatelessWidget {
           crossAxisSpacing: 16,
           mainAxisSpacing: 16,
         ),
-        itemCount: 100,
-        itemBuilder: (_, __) {
-          return Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(16),
-              ),
-              image: DecorationImage(
-                image: NetworkImage(
-                  'https://www.tving.com/img/tving-favicon-160@3x.png',
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-          );
+        itemCount: photos.length,
+        itemBuilder: (_, index) {
+          final photo = photos[index];
+
+          return PhotoWidget(photo: photo);
         },
       ),
     );
