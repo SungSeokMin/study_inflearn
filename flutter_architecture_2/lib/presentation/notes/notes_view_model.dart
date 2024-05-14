d8b653dc982e204a19bf3c65e0a714a61bf78355
sungseokmin <jkl154527@gmail.com>
2024-05-14 14:09:36 +0900

    7. 첫 번째 ViewModel 작성

---
+++ flutter_architecture_2/lib/presentation/notes/notes_view_model.dart
@@ -0,0 +1,52 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_architecture/domain/model/note.dart';
+import 'package:flutter_architecture/domain/repository/note_repository.dart';
+import 'package:flutter_architecture/presentation/notes/notes_event.dart';
+
+class NotesViewModel with ChangeNotifier {
+  final NoteRepository repository;
+
+  NotesViewModel({
+    required this.repository,
+  });
+
+  List<Note> _notes = [];
+
+  UnmodifiableListView<Note> get notes => UnmodifiableListView(_notes);
+
+  Note? _recentlyDeletedNote;
+
+  void onEvent(NotesEvent event) {
+    event.when(
+      loadNotes: _loadNotes,
+      deleteNote: _deleteNote,
+      restoreNote: _restoreNote,
+    );
+  }
+
+  Future<void> _loadNotes() async {
+    List<Note> notes = await repository.getNotes();
+    _notes = notes;
+
+    notifyListeners();
+  }
+
+  Future<void> _deleteNote(Note note) async {
+    await repository.deleteNote(note);
+
+    _recentlyDeletedNote = note;
+
+    await _loadNotes();
+  }
+
+  Future<void> _restoreNote() async {
+    if (_recentlyDeletedNote != null) {
+      await repository.insertNote(_recentlyDeletedNote!);
+      _recentlyDeletedNote = null;
+
+      _loadNotes();
+    }
+  }
+}
