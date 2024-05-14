11582c6ee5ca5f6030e2f13240e7f3ca1cb49644
sungseokmin <jkl154527@gmail.com>
2024-05-14 17:50:55 +0900

    10. 첫 화면 UI 작성

--- flutter_architecture_2/lib/main.dart
+++ flutter_architecture_2/lib/main.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture/presentation/notes/notes_screen.dart';
+import 'package:flutter_architecture/ui/colors.dart';
 
 void main() {
-  return runApp(const MaterialApp(
-    home: HomeScreen(),
-  ));
+  runApp(
+    const MyApp(),
+  );
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: darkGray,
+        floatingActionButtonTheme: Theme.of(context).floatingActionButtonTheme.copyWith(
+              backgroundColor: lightBlue,
+              foregroundColor: darkGray,
+            ),
+        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+              backgroundColor: darkGray,
+              foregroundColor: lightBlue,
+            ),
+      ),
+      home: const NotesScreen(),
+    );
   }
 }
