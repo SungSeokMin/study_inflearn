28cf269aa09e4ed56c8b21debaebe3b9762bd531
sungseokmin <jkl154527@gmail.com>
2024-05-23 17:19:38 +0900

    패래럴 라우트

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Link from "next/link";
-import zLogo from "../../public/zlogo.png";
+
+import styles from "../page.module.css";
+
+import zLogo from "../../../public/zlogo.png";
 
 export default function Home() {
   return (
@@ -12,7 +14,7 @@ export default function Home() {
 
       <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
-        
+
         <h2>지금 가입하세요.</h2>
 
         <Link href="/i/flow/signup" className={styles.signup}>
@@ -21,7 +23,7 @@ export default function Home() {
 
         <h3>이미 트위터에 가입하셨나요?</h3>
 
-        <Link href="/login" className={styles.login}>
+        <Link href="/i/flow/login" className={styles.login}>
           로그인
         </Link>
       </div>
