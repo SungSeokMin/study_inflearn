5d80bf2378c07509f60f3606ce5cf2f396a43595
sungseokmin <jkl154527@gmail.com>
2024-05-28 13:33:05 +0900

    게시글 만들며 dayjs 사용해보기

--- nextjs/src/app/(afterLogin)/home/page.tsx
+++ nextjs/src/app/(afterLogin)/home/page.tsx
@@ -1,6 +1,7 @@
 import Tab from "@/app/(afterLogin)/home/_component/tab/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/tabProvider/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/postForm/PostForm";
+import Post from "@/app/(afterLogin)/_component/post/Post";
 
 import styles from "./home.module.css";
 
@@ -12,7 +13,7 @@ const HomePage = () => {
 
         <PostForm />
 
-        {/*<Post/>*/}
+        <Post />
         {/*<Post/>*/}
         {/*<Post/>*/}
         {/*<Post/>*/}
