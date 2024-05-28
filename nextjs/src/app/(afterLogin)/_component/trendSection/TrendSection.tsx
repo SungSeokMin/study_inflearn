1c3a13249a6bf4c55d7db6178f5b2c859109e7cd
sungseokmin <jkl154527@gmail.com>
2024-05-28 15:56:34 +0900

    usePathname과 /explore 페이지

--- nextjs/src/app/(afterLogin)/_component/trendSection/TrendSection.tsx
+++ nextjs/src/app/(afterLogin)/_component/trendSection/TrendSection.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import Trend from "@/app/(afterLogin)/_component/trendSection/Trend";
 import styles from "./trendSection.module.css";
+import { usePathname } from "next/navigation";
 
 interface TrendSectionProps {}
 
 const TrendSection = ({}: TrendSectionProps) => {
+  const pathname = usePathname();
+
+  if (pathname === "/explore") return null;
+
   return (
     <div className={styles.trendBg}>
       <div className={styles.trend}>
--- nextjs/src/app/(afterLogin)/explore/page.tsx
+++ nextjs/src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,28 @@
+import SearchForm from "@/app/(afterLogin)/_component/searchForm/SearchForm";
+import Trend from "@/app/(afterLogin)/_component/trendSection/Trend";
+
+import styles from "./explore.module.css";
+
 const ExplorePage = () => {
   return (
-    <div>Explore Page</div>
+    <main className={styles.main}>
+      <div className={styles.formZone}>
+        <SearchForm />
+      </div>
+
+      <div className={styles.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
   );
-}
+};
 
 export default ExplorePage;
