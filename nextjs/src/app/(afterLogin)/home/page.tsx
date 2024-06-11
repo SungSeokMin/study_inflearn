4ee5ccd9f277fb61d3d8d837ab3ae35055d5f39f
sungseokmin <jkl154527@gmail.com>
2024-06-11 17:26:01 +0900

    react-query SSR 설정하기

--- nextjs/src/app/(afterLogin)/home/page.tsx
+++ nextjs/src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,9 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
 import Tab from "@/app/(afterLogin)/home/_component/tab/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/tabProvider/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/postForm/PostForm";
@@ -5,27 +11,53 @@ import Post from "@/app/(afterLogin)/_component/post/Post";
 
 import styles from "./home.module.css";
 
-const HomePage = () => {
+const getPostRecommends = async () => {
+  const response = await fetch("http://localhost:9090/api/postRecommends", {
+    next: {
+      tags: ["posts", "recommends"],
+    },
+    cache: "no-cache",
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
+};
+
+const HomePage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <main className={styles.main}>
-      <TabProvider>
-        <Tab />
+      <HydrationBoundary state={dehydratedState}>
+        <TabProvider>
+          <Tab />
 
-        <PostForm />
+          <PostForm />
 
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </TabProvider>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
 };
