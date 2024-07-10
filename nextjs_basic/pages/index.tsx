850cdc998297570deeae7511c33a6e79541e0061
sungseokmin <jkl154527@gmail.com>
2024-07-10 15:11:38 +0900

    리액트 문법만으로 화면에 데이터 표시하기

--- nextjs_basic/pages/index.tsx
+++ nextjs_basic/pages/index.tsx
@@ -1,4 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+interface Product {
+	id: string;
+	name: string;
+	price: string;
+	imageUrl: string;
+}
+
 function ProductPage() {
+	const [products, setProducts] = useState<null | Product[]>(null);
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:4000/products')
+			.then(response => setProducts(response.data));
+	}, []);
+
 	return <div>상품 목록</div>;
 }
 
