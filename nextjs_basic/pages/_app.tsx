3ec614d35f042cb16b5eaf7df2bcca9220871f08
sungseokmin <jkl154527@gmail.com>
2024-07-10 10:48:19 +0900

    커스텀 앱(_app 파일) 컴포넌트 소개

--- nextjs_basic/pages/_app.tsx
+++ nextjs_basic/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<div>
+			<nav>
+				<div>
+					<Link href="/home">Home</Link>
+				</div>
+
+				<div>
+					<Link href="/login">Login</Link>
+				</div>
+			</nav>
+
+			<Component {...pageProps} />
+		</div>
+	);
 }
