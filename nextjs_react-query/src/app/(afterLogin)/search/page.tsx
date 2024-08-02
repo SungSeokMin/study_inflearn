c4f7bab8ed0bda027854958f5d4b8892fbca9e3d
sungseokmin <jkl154527@gmail.com>
2024-08-02 11:13:56 +0900

    useSearchParams와 프로필, /search 페이지

--- nextjs_react-query/src/app/(afterLogin)/search/page.tsx
+++ nextjs_react-query/src/app/(afterLogin)/search/page.tsx
@@ -1,7 +1,45 @@
-type Props = {};
+import BackButton from '../_component/BackButton';
+import SearchForm from '../_component/SearchForm';
 
-const SearchPage = ({}: Props) => {
-	return <div>SearchPage</div>;
+import Post from '../_component/Post';
+import Tab from './_component/Tab';
+
+import style from './search.module.css';
+
+type Props = {
+	searchParams: { q: string };
+};
+
+const SearchPage = ({ searchParams }: Props) => {
+	return (
+		<main className={style.main}>
+			<div className={style.searchTop}>
+				<div className={style.searchZone}>
+					<div className={style.buttonZone}>
+						<BackButton />
+					</div>
+					<div className={style.formZone}>
+						<SearchForm q={searchParams.q} />
+					</div>
+				</div>
+				<Tab />
+			</div>
+			<div className={style.list}>
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				{/*<SearchResult searchParams={searchParams} />*/}
+			</div>
+		</main>
+	);
 };
 
 export default SearchPage;
