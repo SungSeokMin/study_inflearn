ab62fd7eba0ecfddfbabbc0c0cd9c5f44b164a25
sungseokmin <jkl154527@gmail.com>
2024-08-05 15:51:26 +0900

    /messages 페이지 라이브 클론

--- nextjs_react-query/src/app/(afterLogin)/messages/page.tsx
+++ nextjs_react-query/src/app/(afterLogin)/messages/page.tsx
@@ -1,7 +1,22 @@
+import Room from './_component/Room';
+import style from './message.module.css';
+
 type Props = {};
 
 const MessagesPage = ({}: Props) => {
-	return <div>MessagesPage</div>;
+	return (
+		<main className={style.main}>
+			<div className={style.header}>
+				<h3>쪽지</h3>
+			</div>
+			<Room />
+			<Room />
+			<Room />
+			<Room />
+			<Room />
+			<Room />
+		</main>
+	);
 };
 
 export default MessagesPage;
