d3d75c7665005bb6b8574237d589993a5f65a218
sungseokmin <jkl154527@gmail.com>
2024-08-05 16:08:34 +0900

    다중 이미지 구역 만들기

--- nextjs_react-query/src/app/(afterLogin)/_component/Post.tsx
+++ nextjs_react-query/src/app/(afterLogin)/_component/Post.tsx
@@ -8,6 +8,7 @@ import 'dayjs/locale/ko';
 
 import ActionButtons from './ActionButtons';
 import PostArticle from './PostArticle';
+import PostImages from './PostImages';
 
 import { PostType } from '../_type/post.types';
 
@@ -58,15 +59,8 @@ const Post = ({ noImage }: Props) => {
 					</div>
 					<div>{target.content}</div>
 
-					<div className={style.postImageSection}>
-						{target.Images && target.Images.length > 0 && (
-							<Link
-								href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-								className={style.postImageSection}
-							>
-								<img src={target.Images[0]?.link} alt="" />
-							</Link>
-						)}
+					<div>
+						<PostImages post={target} />
 					</div>
 					<ActionButtons />
 				</div>
