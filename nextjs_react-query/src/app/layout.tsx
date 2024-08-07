3dfe3481e854d8e08d8d07746b1455e638d92fa5
sungseokmin <jkl154527@gmail.com>
2024-08-07 10:51:30 +0900

    next용 msw 컴포넌트와 .env

--- nextjs_react-query/src/app/layout.tsx
+++ nextjs_react-query/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 
 import '@/app/globals.css';
+import MSWComponent from './_component/MSWComponent';
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<MSWComponent />
+				{children}
+			</body>
 		</html>
 	);
 }
